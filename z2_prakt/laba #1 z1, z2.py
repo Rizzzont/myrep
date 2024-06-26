ac47f4f68e92cbaa540a30a9cbd3e5ea424b0fb4
lesha <rizzzont@gmail.com>
2024-06-26 17:29:21 +0300

    Заголовок * Супер обновление * Добавление фишки * Какой то текст

---
+++ z2_prakt/laba #1 z1, z2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.array([[1, 2, 3],
+              [3, 2, 1],
+              [2, 3, 1]])
+V = np.array([[1],
+              [1],
+              [1]])
+
+L = np.dot(A, V)
+print(f'Умножение:\n{L}')
